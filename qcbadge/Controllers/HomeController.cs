23bc3b421b0d08994b44ed28a6326d4c3b26809b
Jake Visser <visser.jacob@outlook.com>
2017-07-05 21:16:00 -0700

    List controller

--- qcbadge/Controllers/HomeController.cs
+++ qcbadge/Controllers/HomeController.cs
@@ -50,7 +50,47 @@ namespace qcbadge.Controllers
 
         }
 
+        public IActionResult List(string s, string id, int refresh)
+        {
+            Helpers.Sql sql = new Helpers.Sql();
+            ViewData["refresh"] = refresh;
 
+            if ((String.Compare(Startup.scode, s, true) == 0))
+            {
+                ViewData["Message"] = "";
+                ViewData["0"] = 0;
+                ViewData["1"] = 0;
+                ViewData["38"] = 0;
+
+                bool[] imglist = new bool[50];
+
+                if (String.IsNullOrEmpty(id))
+                {
+                    imglist = sql.selectGlobalView();
+
+                    for (int i = 0; i < 50; i++)
+                    {
+                        ViewData[i.ToString()] = imglist[i];
+                    }
+                }
+                else
+                {
+                    int badgeid = Convert.ToInt32(id);
+                    badgeid = badgeid - 1;
+                    imglist = sql.selectIndervidualView(badgeid);
+
+                    for (int i = 0; i < 50; i++)
+                    {
+                        ViewData[i.ToString()] = imglist[i];
+                    }
+
+                }
+
+                return View();
+            }
+            else { return StatusCode(401); }
+
+        }
         public IActionResult Error()
         {
             return View();
